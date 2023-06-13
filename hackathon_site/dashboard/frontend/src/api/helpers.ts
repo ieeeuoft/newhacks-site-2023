ab0461cddd3c75be214e4f05b5bdc8ca1ea0ff1e
Mustafa <mus2003.abdul@gmail.com>
2023-06-13 19:20:58 -0400

    Initial commit

---
+++ hackathon_site/dashboard/frontend/src/api/helpers.ts
@@ -0,0 +1,96 @@
+import {
+    Order,
+    OrderInTable,
+    OrderItemTableRow,
+    ReturnedItem,
+    ReturnOrderInTable,
+} from "api/types";
+
+export const formatDateTime = (dateTimeString: string): string => {
+    const dateTime = new Date(dateTimeString);
+    const options: Intl.DateTimeFormatOptions = {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    };
+    return dateTime.toLocaleString("en-US", options);
+};
+export const teamOrderListSerialization = (
+    orders: Order[]
+): {
+    pendingOrders: OrderInTable[];
+    checkedOutOrders: OrderInTable[];
+    returnedOrders: ReturnOrderInTable[];
+    hardwareIdsToFetch: number[];
+} => {
+    const pendingOrders: OrderInTable[] = [];
+    const checkedOutOrders: OrderInTable[] = [];
+    const returnedOrders: ReturnOrderInTable[] = [];
+    const hardwareIdsToFetch: Record<number, number> = {};
+    orders.forEach((order) => {
+        if (order.status !== "Cancelled") {
+            const hardwareItems: Record<number, OrderItemTableRow> = {};
+            const hardwareRequested: Record<number, number> = {};
+            const returnedItems: Record<string, ReturnedItem> = {};
+            order.request.forEach(
+                (hardware) =>
+                    (hardwareRequested[hardware.id] = hardware.requested_quantity)
+            );
+            order.items.forEach(({ id, hardware_id, part_returned_health }) => {
+                if (part_returned_health) {
+                    const returnItemKey = `${hardware_id}-${part_returned_health}`;
+                    if (returnedItems[returnItemKey])
+                        returnedItems[returnItemKey].quantity += 1;
+                    else {
+                        const date = new Date(order.updated_at);
+                        returnedItems[returnItemKey] = {
+                            id,
+                            quantity: 1,
+                            part_returned_health,
+                            hardware_id,
+                            time: `${date.toLocaleTimeString()} (${date.toDateString()})`,
+                        };
+                    }
+                } else {
+                    if (hardwareItems[hardware_id])
+                        hardwareItems[hardware_id].quantityGranted += 1;
+                    else
+                        hardwareItems[hardware_id] = {
+                            id: hardware_id,
+                            quantityGranted: 1,
+                            quantityRequested: hardwareRequested[hardware_id],
+                        };
+                }
+                hardwareIdsToFetch[hardware_id] = hardware_id;
+            });
+            const returnedHardware = Object.values(returnedItems);
+            if (returnedHardware.length)
+                returnedOrders.push({
+                    id: order.id,
+                    hardwareInOrder: returnedHardware,
+                });
+
+            const hardwareInTableRow = Object.values(hardwareItems);
+            if (hardwareInTableRow.length > 0)
+                (order.status === "Submitted" || order.status === "Ready for Pickup"
+                    ? pendingOrders
+                    : checkedOutOrders
+                ).push({
+                    id: order.id,
+                    status: order.status,
+                    hardwareInTableRow,
+                    createdTime: order.created_at,
+                    updatedTime: order.updated_at,
+                });
+        }
+    });
+    return {
+        pendingOrders: [...pendingOrders].sort((a, b) => b.id - a.id),
+        checkedOutOrders: [...checkedOutOrders].sort((a, b) => b.id - a.id),
+        returnedOrders,
+        hardwareIdsToFetch: Object.values(hardwareIdsToFetch),
+    };
+};
---
+++ hackathon_site/dashboard/frontend/src/components/dashboard/ItemTable/ItemTable.tsx
@@ -0,0 +1,378 @@
+import React, { useState } from "react";
+import styles from "components/general/OrderTables/OrderTables.module.scss";
+import {
+    Button,
+    Paper,
+    Container,
+    IconButton,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from "@material-ui/core";
+import Info from "@material-ui/icons/Info";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    isCheckedOutTableVisibleSelector,
+    isPendingTableVisibleSelector,
+    isReturnedTableVisibleSelector,
+    openProductOverview,
+    toggleCheckedOutTable,
+    togglePendingTable,
+    toggleReturnedTable,
+} from "slices/ui/uiSlice";
+import {
+    getUpdatedHardwareDetails,
+    hardwareSelectors,
+} from "slices/hardware/hardwareSlice";
+import hardwareImagePlaceholder from "assets/images/placeholders/no-hardware-image.svg";
+import {
+    checkedOutOrdersSelector,
+    orderErrorSelector,
+    pendingOrderSelectors,
+    returnedOrdersSelector,
+    cancelOrderThunk,
+    cancelOrderLoadingSelector,
+} from "slices/order/orderSlice";
+import {
+    GeneralOrderTitle,
+    GeneralOrderTableTitle,
+    GeneralPendingTable,
+    GeneralReturnTable,
+} from "components/general/OrderTables/OrderTables";
+import PopupModal from "components/general/PopupModal/PopupModal";
+
+export const CheckedOutTables = () =>
+    // TODO: for incident reports
+    // { push,
+    // reportIncident, }
+    {
+        const dispatch = useDispatch();
+        const orders = useSelector(checkedOutOrdersSelector);
+        const hardware = useSelector(hardwareSelectors.selectEntities);
+        const isVisible = useSelector(isCheckedOutTableVisibleSelector);
+        const fetchOrdersError = useSelector(orderErrorSelector);
+        const toggleVisibility = () => dispatch(toggleCheckedOutTable());
+        const openProductOverviewPanel = (hardwareId: number) => {
+            dispatch(getUpdatedHardwareDetails(hardwareId));
+            dispatch(openProductOverview());
+        };
+
+        return (
+            <Container
+                className={styles.tableContainer}
+                maxWidth={false}
+                disableGutters={true}
+            >
+                <GeneralOrderTitle
+                    {...{
+                        title: "Checked out items",
+                        isVisible,
+                        toggleVisibility,
+                    }}
+                />
+
+                {isVisible &&
+                    (!orders.length || fetchOrdersError ? (
+                        <Paper elevation={2} className={styles.empty} square={true}>
+                            {fetchOrdersError
+                                ? `Unable to view checked out items.`
+                                : "You have no items checked out yet. View our inventory."}
+                        </Paper>
+                    ) : (
+                        orders.map((checkedOutOrder) => (
+                            <div
+                                id={`order${checkedOutOrder.id}`}
+                                key={checkedOutOrder.id}
+                            >
+                                <GeneralOrderTableTitle
+                                    orderId={checkedOutOrder.id}
+                                    createdTime={checkedOutOrder.createdTime}
+                                    updatedTime={checkedOutOrder.updatedTime}
+                                    additionalChipFormatting={true}
+                                />
+                                <TableContainer
+                                    component={Paper}
+                                    elevation={2}
+                                    square={true}
+                                >
+                                    <Table
+                                        className={styles.table}
+                                        size="small"
+                                        aria-label="checkout table"
+                                    >
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell
+                                                    className={styles.widthFixed}
+                                                />
+                                                <TableCell
+                                                    className={styles.width6}
+                                                    align="left"
+                                                >
+                                                    Name
+                                                </TableCell>
+                                                <TableCell
+                                                    className={styles.widthFixed}
+                                                    align="center"
+                                                >
+                                                    Info
+                                                </TableCell>
+                                                <TableCell
+                                                    className={styles.widthFixed}
+                                                    align="right"
+                                                >
+                                                    Qty
+                                                </TableCell>
+                                                <TableCell
+                                                    align="right"
+                                                    className={styles.width6}
+                                                />
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {checkedOutOrder.hardwareInTableRow.map(
+                                                (row) => (
+                                                    <TableRow
+                                                        key={row.id}
+                                                        data-testid={`checked-out-hardware-${row.id}-${checkedOutOrder.id}`}
+                                                    >
+                                                        <TableCell align="left">
+                                                            <img
+                                                                className={
+                                                                    styles.itemImg
+                                                                }
+                                                                src={
+                                                                    hardware[row.id]
+                                                                        ?.picture ??
+                                                                    hardware[row.id]
+                                                                        ?.image_url ??
+                                                                    hardwareImagePlaceholder
+                                                                }
+                                                                alt={
+                                                                    hardware[row.id]
+                                                                        ?.name
+                                                                }
+                                                            />
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            {hardware[row.id]?.name}
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            <IconButton
+                                                                color="inherit"
+                                                                aria-label="Info"
+                                                                data-testid="info-button"
+                                                                onClick={() =>
+                                                                    openProductOverviewPanel(
+                                                                        row.id
+                                                                    )
+                                                                }
+                                                            >
+                                                                <Info />
+                                                            </IconButton>
+                                                        </TableCell>
+                                                        <TableCell align="right">
+                                                            {row.quantityGranted}
+                                                        </TableCell>
+                                                        <TableCell align="right">
+                                                            {/* TODO: Add back in when incident reports are being used*/}
+                                                            {/*<Button*/}
+                                                            {/*    color="secondary"*/}
+                                                            {/*    size="small"*/}
+                                                            {/*    onClick={() => {*/}
+                                                            {/*        reportIncident(row.id);*/}
+                                                            {/*        push("/incident-form");*/}
+                                                            {/*    }}*/}
+                                                            {/*>*/}
+                                                            {/*    Report broken/lost*/}
+                                                            {/*</Button>*/}
+                                                        </TableCell>
+                                                    </TableRow>
+                                                )
+                                            )}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </div>
+                        ))
+                    ))}
+            </Container>
+        );
+    };
+
+export const ReturnedTable = () => {
+    const dispatch = useDispatch();
+    const orders = useSelector(returnedOrdersSelector);
+    const fetchOrdersError = useSelector(orderErrorSelector);
+    const isVisible = useSelector(isReturnedTableVisibleSelector);
+    const toggleVisibility = () => dispatch(toggleReturnedTable());
+
+    return (
+        <GeneralReturnTable
+            {...{
+                orders,
+                fetchOrdersError,
+                isVisible,
+                toggleVisibility,
+            }}
+        />
+    );
+};
+
+export const PendingTables = () => {
+    const dispatch = useDispatch();
+    const orders = useSelector(pendingOrderSelectors.selectAll);
+    const isVisible = useSelector(isPendingTableVisibleSelector);
+    const isCancelOrderLoading = useSelector(cancelOrderLoadingSelector);
+    const toggleVisibility = () => dispatch(togglePendingTable());
+    const cancelOrder = (orderId: number) => dispatch(cancelOrderThunk(orderId));
+    const [showCancelOrderModal, setShowCancelOrderModal] = useState(false);
+
+    const closeModal = () => {
+        setShowCancelOrderModal(false);
+    };
+
+    const submitModal = (cancelOrderId: number) => {
+        cancelOrder(cancelOrderId); // Perform Cancellation
+        setShowCancelOrderModal(false);
+    };
+
+    return (
+        <Container
+            className={styles.tableContainer}
+            maxWidth={false}
+            disableGutters={true}
+        >
+            {orders.length > 0 && (
+                <GeneralOrderTitle
+                    {...{
+                        title: "Pending Orders",
+                        isVisible,
+                        toggleVisibility,
+                    }}
+                />
+            )}
+
+            {isVisible &&
+                orders.length > 0 &&
+                orders.map((pendingOrder) => (
+                    <div
+                        id={`order${pendingOrder.id}`}
+                        key={pendingOrder.id}
+                        data-testid={`pending-order-table-${pendingOrder.id}`}
+                    >
+                        <GeneralPendingTable {...{ pendingOrder }} />
+                        {pendingOrder.status !== "Ready for Pickup" && (
+                            <div
+                                style={{
+                                    display: "flex",
+                                    justifyContent: "flex-end",
+                                    marginTop: "10px",
+                                }}
+                            >
+                                <Button
+                                    onClick={() => setShowCancelOrderModal(true)}
+                                    disabled={isCancelOrderLoading}
+                                    color="secondary"
+                                    data-testid="cancel-order-button"
+                                >
+                                    Cancel order
+                                </Button>
+                                <PopupModal
+                                    description={
+                                        "Are you sure you want to cancel this order? The team will be notified."
+                                    }
+                                    isVisible={showCancelOrderModal}
+                                    submitHandler={() => submitModal(pendingOrder.id)}
+                                    cancelText={"Go Back"}
+                                    submitText={"Delete Order"}
+                                    cancelHandler={closeModal}
+                                    title={"Careful!"}
+                                />
+                            </div>
+                        )}
+                    </div>
+                ))}
+        </Container>
+    );
+};
+
+// TODO: implement when incidents are finished
+// export const BrokenTable = ({ items, openReportAlert }) => {
+//     return !items.length ? null : (
+//         <Container
+//             className={styles.tableContainer}
+//             maxWidth={false}
+//             disableGutters={true}
+//         >
+//             <div className={styles.titleChip}>
+//                 <Typography
+//                     variant="h2"
+//                     className={styles.titleChipText}
+//                     color="secondary"
+//                 >
+//                     Reported broken/lost items
+//                 </Typography>
+//                 <ChipStatus status="error" />
+//             </div>
+//
+//             <TableContainer component={Paper} elevation={2} square={true}>
+//                 <Table className={styles.table} size="small" aria-label="broken table">
+//                     <TableHead>
+//                         <TableRow>
+//                             <TableCell className={styles.widthFixed} />
+//                             <TableCell className={styles.width6} align="left">
+//                                 Name
+//                             </TableCell>
+//                             <TableCell className={styles.widthFixed} align="left">
+//                                 Qty
+//                             </TableCell>
+//                             <TableCell className={styles.width4} align="right">
+//                                 Time
+//                             </TableCell>
+//                             <TableCell className={styles.width2} align="left">
+//                                 Condition
+//                             </TableCell>
+//                             <TableCell className={styles.widthFixed} />
+//                         </TableRow>
+//                     </TableHead>
+//                     <TableBody>
+//                         {items.map((row) => (
+//                             <TableRow key={row.id}>
+//                                 <TableCell align="left">
+//                                     <img
+//                                         className={styles.itemImg}
+//                                         src={row.url}
+//                                         alt={row.name}
+//                                     />
+//                                 </TableCell>
+//                                 <TableCell align="left">{row.name}</TableCell>
+//                                 <TableCell align="right">{row.qty}</TableCell>
+//                                 <TableCell align="right" className={styles.noWrap}>
+//                                     {row.time}
+//                                 </TableCell>
+//                                 <TableCell align="left" className={styles.noWrap}>
+//                                     {row.condition}
+//                                 </TableCell>
+//                                 <TableCell align="right">
+//                                     <Button
+//                                         color="primary"
+//                                         size="small"
+//                                         onClick={() => {
+//                                             openReportAlert(row.id);
+//                                         }}
+//                                     >
+//                                         View Report
+//                                     </Button>
+//                                 </TableCell>
+//                             </TableRow>
+//                         ))}
+//                     </TableBody>
+//                 </Table>
+//             </TableContainer>
+//         </Container>
+//     );
+// };
