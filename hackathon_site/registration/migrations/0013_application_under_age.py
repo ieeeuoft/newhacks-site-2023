a81a2762ac35e0f85adf22957b75dcc1f1e31b86
Thomas <37830237+thomaslin2020@users.noreply.github.com>
2023-10-23 21:08:24 -0400

    Update 0013_application_under_age.py

--- hackathon_site/registration/migrations/0013_application_under_age.py
+++ hackathon_site/registration/migrations/0013_application_under_age.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("registration", "0011_auto_20230627_2303"),
+        ("registration", "0012_application_rsvp"),
     ]
 
     operations = [
